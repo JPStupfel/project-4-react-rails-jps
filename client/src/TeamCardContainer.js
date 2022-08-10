ece8e57fb21e68939dbb2c52ea241a61d6e6821b
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-10 14:47:16 -0500

    cards stack 2-4-6 deep based on zoom

--- client/src/TeamCardContainer.js
+++ client/src/TeamCardContainer.js
@@ -1,16 +1,26 @@
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import UserCard from './UserCard';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 function TeamCardContainer({teamList}) {
 
-  const cards = teamList.map(e=><UserCard key={e.id} user={e}/>)
+  const cards = teamList.map(
+    e=>
+    
+    <UserCard key={e.id} user={e}
+    
+    />)
     
   return (
 
     <CardGroup>
-
+      <Row xs={2} md={4} lg={6} className="g-4">
       {cards}
+      
+      </Row>
 
 
     </CardGroup>
