d75fc75bc435b8e0540ddadea2061dc62c6da137
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-10 12:19:22 -0500

    database created and users migrated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times.do User.create(
-    name: Faker::Games::StreetFighter.character,
-    bio: "Quote: #{Faker::Games::StreetFighter.quote}. Birthplace: #{Faker::Games::StreetFighter.stage}",
-    image: Faker::Avatar.image,
-    age: rand(100)
-)
+# 10.times.do User.create(
+#     name: Faker::Games::StreetFighter.character,
+#     bio: "Quote: #{Faker::Games::StreetFighter.quote}. Birthplace: #{Faker::Games::StreetFighter.stage}",
+#     image: Faker::Avatar.image,
+#     age: rand(100)
+# )
