75c9edc976748f0ee408d3d21ed35c60e110dd6e
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-10 15:19:17 -0500

    login form is controlled form

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,20 +1,38 @@
+
+import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function Login() {
+
+    const [cred, setCred] = useState({password: '', username: ''})
+
+    function handleChange(event){
+        event.preventDefault()
+        let newCred = {...cred}
+        newCred[event.target.id] = event.target.value
+        setCred(newCred)
+    }
+    
   return (
     <Form style={{'maxWidth': '50%'}}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+
+      <Form.Group className="mb-3" controlId="username">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="username" placeholder="Enter username" />
+        <Form.Control 
+        
+        onChange={handleChange} 
+        type="username" 
+        placeholder="Enter username" />
         <Form.Text className="text-muted">
         
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="password">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control 
+                onChange={handleChange} type="password" placeholder="Password" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
       </Form.Group>
