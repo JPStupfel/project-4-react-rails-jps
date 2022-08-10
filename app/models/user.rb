da3d28a1f2c791e231886892a9b333437ad5b7f6
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-10 12:33:25 -0500

    user validates added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
     # has_many :tasks
     # has_many :projects, through: :tasks
+
+    # validate each of the following attributes
+
+    attributes = [:name, :image, :bio]
+     attributes.each do |e|
+        validates e, presence: true
+     end
+
+     validates :age, numericality: { greater_than_or_equal_to: 0 }
+
 end
