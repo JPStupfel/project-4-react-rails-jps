e0d82c2c32ea8b1a06468634628a32ddc3b1d65b
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-11 12:51:56 -0500

    can signup user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
     def index
         render json: User.all, status: 200
     end
+
+    def create
+        user = User.create user_params
+        if user.valid?
+            render json: user, status: 201
+        else
+            byebug
+            render json: {errors: user.errors.full_error_messages}, status: 422
+        end
+    end
+
+    private
+    def user_params
+        params.permit :username, :bio, :image, :age, :password, :password_confirmation
+    end
 end
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -6,10 +6,10 @@ import Form from 'react-bootstrap/Form';
 
 function Signup({setCurrentUser}) {
     const navigate = useNavigate()
-    const [newUser, setNewUser] = useState({username: null, bio:null,image:null,age:null,password:null})
+    const [newUser, setNewUser] = useState({username: null, bio:null,image:null,age:null,password:null,password_confirmation:null})
 
 
-
+    // dry versino of each input based on all the keys in newUser
     const forms = Object.keys(newUser).map(e=>{
        return ( <Form.Group className="mb-3" controlId={e}>
         <Form.Label>{e}</Form.Label>
@@ -33,7 +33,6 @@ function Signup({setCurrentUser}) {
 
     function handleSubmit(event){
         event.preventDefault()
-
                     fetch('/users', {
             method: 'POST', // or 'PUT'
             headers: {
@@ -55,28 +54,9 @@ function Signup({setCurrentUser}) {
   return (
     <Form onSubmit={handleSubmit} style={{'maxWidth': '50%'}}>
         {forms}
-      {/* <Form.Group className="mb-3" controlId="username">
-        <Form.Label>Username</Form.Label>
-        <Form.Control 
-        onChange={handleChange} 
-        type="username" 
-        placeholder="Enter username" />
-        <Form.Text className="text-muted">
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control 
-                onChange={handleChange} type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      </Form.Group> */}
-
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
     </Form>
 
   );
