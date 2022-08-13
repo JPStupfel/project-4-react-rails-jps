81062b12f0e1ba47850daf72813fcbdb7aa11f39
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-13 06:06:28 -0500

    Project Serializer has tasks remaining summary

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,10 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :details, :duedate, :budget
+attributes :id, :name, :details, :duedate, :budget, :tasksRemaining
+
+def tasksRemaining
+  tasksToGo = self.object.tasks.where(is_complete: false).count
+  {remaining:tasksToGo ,total: self.object.tasks.count}
+end
 
  
 end
