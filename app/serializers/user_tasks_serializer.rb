c689dac89c4244e5acff02d77e1e3307fea8f0cd
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-13 06:51:22 -0500

    user task serializer for user show page has projects method defined for unique projects

--- app/serializers/user_tasks_serializer.rb
+++ app/serializers/user_tasks_serializer.rb
@@ -1,6 +1,10 @@
 class UserTasksSerializer < ActiveModel::Serializer
-  attributes :id, :username, :bio, :image, :age
+  attributes :id, :username, :bio, :image, :age, :projects
+
+  def projects
+    self.object.projects.uniq {|p| p.id}
+  end
 
   has_many :tasks
-  has_many :projects
+  # has_many :projects
 end
