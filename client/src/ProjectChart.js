2cc10ae20717b7380433d1a4dc9bbd4152b4461e
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-17 15:12:30 -0500

    project chart has data

--- client/src/ProjectChart.js
+++ client/src/ProjectChart.js
@@ -10,52 +10,21 @@ import {
   Legend
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
 
-export default function ProjectChart() {
+export default function ProjectChart({project}) {
+
+// const completedTaskCount = Object.keys(projectList.filter(e=> e.tasksRemaining['remaining'] == 0)).length
+
+const data = [
+    {
+      name: "Stats",
+      Users: 500,
+      Projects: 500,
+      Completed_Projects: 500
+    //   Active:  Object.keys(teamList.filter(e=>Object.keys(e.tasks).length >=1)).length,
+      
+    }
+  ];
   return (
     <BarChart
       width={500}
@@ -74,8 +43,9 @@ export default function ProjectChart() {
       <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
       <Tooltip />
       <Legend />
-      <Bar yAxisId="left" dataKey="pv" fill="#8884d8" />
-      <Bar yAxisId="right" dataKey="uv" fill="#82ca9d" />
+      <Bar yAxisId="left" dataKey="Users" fill="#8884d8" />
+      <Bar yAxisId="right" dataKey="Projects" fill="#82ca9d" />
+      <Bar yAxisId="right" dataKey="Completed_Projects" fill="#800000" />
     </BarChart>
   );
 }
